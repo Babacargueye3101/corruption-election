6d837eaf2428e43aff115d38674eee10c0cbb173
Babacar Gueye Root <babacargueye3101@gmail.com>
2025-03-24 14:51:27 +0000

    configue firebase et save all reposne un firebase

--- src/app/pages/components/contact/contact.component.ts
+++ src/app/pages/components/contact/contact.component.ts
@@ -1,11 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      Swal.fire({
+        title: 'Confirmer l\'envoi',
+        text: 'Voulez-vous envoyer votre message ?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'Envoyer',
+        cancelButtonText: 'Annuler'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // Ici vous ajouteriez l'envoi réel au backend
+          Swal.fire(
+            'Envoyé !',
+            'Votre message a bien été transmis à notre équipe.',
+            'success'
+          );
+          this.contactForm.reset();
+        }
+      });
+    }
+  }
 }
